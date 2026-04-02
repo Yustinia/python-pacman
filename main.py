da025b8f031c749e469e26c5eedf1332d6cf95ef
Yustinia <yustinia@protonmail.com>
2026-04-02 23:29:02 +0800

    setup manager class

--- main.py
+++ main.py
@@ -3,8 +3,41 @@ import pygame
 from src.boards import level_one
 
 
+class GameManager:
+    def __init__(self, width: int = 800, height: int = 800, fps: int = 60) -> None:
+        self.width = width
+        self.height = height
+        self.fps = fps
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption("Pacman")
+
+        self.is_running = True
+
+    def event_handler(self) -> None:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.is_running = False
+
+    def update(self) -> None:
+        pass
+
+    def draw(self) -> None:
+        pass
+
+    def runner(self) -> None:
+        while self.is_running:
+            self.event_handler()
+            self.update()
+            self.draw()
+            self.clock.tick(self.fps)
+
+        pygame.quit()
+
+
 def main() -> None:
-    pass
+    gm = GameManager()
+    gm.runner()
 
 
 if __name__ == "__main__":
