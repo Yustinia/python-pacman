8349e400c5ff3fffc7dfbc567ff6ca0779247bd5
Yustinia <yustinia@protonmail.com>
2026-04-02 22:44:23 +0800

    initial

---
+++ justfile
@@ -0,0 +1,7 @@
+# list recipes
+default:
+    just --list
+
+# runner
+run:
+    python3 main.py
---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+
+from src.boards import level_one
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
