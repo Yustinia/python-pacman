6ae1a3e087ab2c766618d49b54e7d8b812855228
Yustinia <yustinia@protonmail.com>
2026-04-03 19:38:10 +0800

    janky movement

--- main.py
+++ main.py
@@ -33,8 +33,87 @@ class Pellet(pygame.sprite.Sprite):
         self.color = color
 
         self.image = pygame.Surface((radius * 2, radius * 2))
-        pygame.draw.circle(self.image, self.color, (radius, radius), radius)
+        pygame.draw.circle(
+            self.image, self.color, (radius, radius), radius, pygame.SRCALPHA
+        )
         self.rect = self.image.get_rect(center=(coord_x, coord_y))
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+
+class Wall(pygame.sprite.Sprite):
+    def __init__(
+        self,
+        coord_x: int,
+        coord_y: int,
+        bx_wd,
+        bx_ht,
+        color: tuple[int, int, int],
+    ) -> None:
+        super().__init__()
+
+        self.coord_x = coord_x
+        self.coord_y = coord_y
+        self.bx_wd = bx_wd
+        self.bx_ht = bx_ht
+        self.color = color
+
+        self.image = pygame.Surface((self.bx_wd, self.bx_ht))
+        pygame.draw.rect(
+            self.image, self.color, (0, 0, self.bx_wd, self.bx_ht), pygame.SRCALPHA
+        )
+        self.rect = self.image.get_rect(center=(self.coord_x, self.coord_y))
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, width, height, bx_wd, bx_ht, color, speed=5) -> None:
+        super().__init__()
+
+        self.width = width
+        self.height = height
+        self.bx_wd = bx_wd
+        self.bx_ht = bx_ht
+        self.color = color
+        self.speed = speed
+
+        self.image = pygame.Surface((self.bx_wd, self.bx_ht))
+        pygame.draw.rect(
+            self.image, self.color, (0, 0, self.bx_wd, self.bx_ht), pygame.SRCALPHA
+        )
+        self.rect = self.image.get_rect(center=(self.width // 2, self.height // 2 - 82))
+
+    def movement(self):
+        keys = pygame.key.get_pressed()
+        self.vx, self.vy = 0, 0
+
+        if keys[pygame.K_a]:
+            self.vx = -self.speed
+        if keys[pygame.K_d]:
+            self.vx = self.speed
+        if keys[pygame.K_w]:
+            self.vy = -self.speed
+        if keys[pygame.K_s]:
+            self.vy = self.speed
+
+    def update(self, wall_grp, pellet_grp, power_pellet_grp):
+        self.movement()
+
+        self.rect.x += self.vx
+        if pygame.sprite.spritecollideany(self, wall_grp):
+            self.rect.x -= self.vx
+
+        self.rect.y += self.vy
+        if pygame.sprite.spritecollideany(self, wall_grp):
+            self.rect.y -= self.vy
+
+        if pygame.sprite.spritecollideany(self, power_pellet_grp):
+            pygame.sprite.spritecollide(self, power_pellet_grp, True)
+
+        pygame.sprite.spritecollide(self, pellet_grp, True)
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
@@ -50,35 +129,63 @@ class Board:
         bg_clr = (10, 10, 20)
         self.background = Background(self.width, self.height, bg_clr)
 
+        wall_clr = (50, 50, 200)
+        self.walls = pygame.sprite.Group()
+
         self.pellets = pygame.sprite.Group()
+        self.power_pellets = pygame.sprite.Group()
         board_wd, board_ht = self.width // len(self.board[0]), self.height // len(
             self.board
         )
+
+        self.player = Player(self.width, self.height, 20, 20, (255, 255, 0))
 
         pellet_color = (200, 200, 200)
         power_pellet_color = (255, 40, 40)
         for i in range(len(self.board)):
             for j in range(len(self.board[i])):
-                pellet_pos = (
+                const_pos = (
                     j * board_wd + board_wd // 2,
                     i * board_ht + board_ht // 2,
                 )
 
                 if self.board[i][j] == 1:
-                    self.pellets.add(Pellet(pellet_pos[0], pellet_pos[1], pellet_color))
+                    self.pellets.add(Pellet(const_pos[0], const_pos[1], pellet_color))
 
                 if self.board[i][j] == 2:
-                    self.pellets.add(
+                    self.power_pellets.add(
                         Pellet(
-                            pellet_pos[0], pellet_pos[1], power_pellet_color, radius=10
+                            const_pos[0], const_pos[1], power_pellet_color, radius=10
                         )
                     )
+
+                if self.board[i][j] == 3:
+                    self.walls.add(
+                        Wall(
+                            const_pos[0],
+                            const_pos[1],
+                            board_wd,
+                            board_ht,
+                            wall_clr,
+                        )
+                    )
+
+    def update(self):
+        self.player.update(self.walls, self.pellets, self.power_pellets)
 
     def draw(self, screen):
         self.background.draw(screen)
 
         for pellet in self.pellets:
             pellet.draw(screen)
+
+        for power_pellet in self.power_pellets:
+            power_pellet.draw(screen)
+
+        for wall in self.walls:
+            wall.draw(screen)
+
+        self.player.draw(screen)
 
 
 class Game:
@@ -88,12 +195,15 @@ class Game:
 
         self.board = Board(self.width, self.height)
 
+    def update(self):
+        self.board.update()
+
     def draw(self, screen):
         self.board.draw(screen)
 
 
 class GameManager:
-    def __init__(self, width: int = 800, height: int = 800, fps: int = 60) -> None:
+    def __init__(self, width: int = 800, height: int = 900, fps: int = 60) -> None:
         self.width = width
         self.height = height
         self.fps = fps
@@ -112,7 +222,7 @@ class GameManager:
                 self.is_running = False
 
     def update(self) -> None:
-        pass
+        self.game.update()
 
     def draw(self) -> None:
         self.game.draw(self.screen)
